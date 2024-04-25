5df90f968bc286d736e8484c100e35d6221df350
Minhaz <minhaz1217@gmail.com>
2024-04-26 02:12:13 +0600

    api working

--- quarkus-getting-started/src/main/java/com/minhazul/BookResource.java
+++ quarkus-getting-started/src/main/java/com/minhazul/BookResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/api/books")
+public class BookResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello World";
     }
 }
