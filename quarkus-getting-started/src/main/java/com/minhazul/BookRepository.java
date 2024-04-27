12bcbb81d6d86881eb6188a75147500410949758
Minhaz <minhaz1217@gmail.com>
2024-04-27 08:35:40 +0600

    29. injecting configuration

--- quarkus-getting-started/src/main/java/com/minhazul/BookRepository.java
+++ quarkus-getting-started/src/main/java/com/minhazul/BookRepository.java
@@ -6,18 +6,24 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
+
+    @ConfigProperty(name="books.genre", defaultValue = "Sci-Fi")
+    String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "Test Book 1", "Random", 2020, "IT"),
-                new Book(2, "Test Book 2", "Random", 2021, "IT"),
-                new Book(3, "Test Book 3", "Random", 2022, "IT"),
-                new Book(4, "Test Book 4", "Random", 2023, "IT")
+                new Book(1, "Test Book 1", "Random", 2020, genre),
+                new Book(2, "Test Book 2", "Random", 2021, genre),
+                new Book(3, "Test Book 3", "Random", 2022, genre),
+                new Book(4, "Test Book 4", "Random", 2023, genre)
         );
     }
 
